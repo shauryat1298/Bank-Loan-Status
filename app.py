af069558269633e07dce2c0f44cd542e7f343ce9
shauryat1298 <shauryat1298@gmail.com>
2023-01-07 16:28:14 +0530

    Basic HTML added

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-
 def predict_api():
     data = request.json['data']
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,6 +20,14 @@ def predict_api():
     op = xgb_model.predict(new_data)
     print(op[0])
     return jsonify(int(op[0]))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    f_ip = scalar.transform(np.array(data).reshape(1,-1))
+    print(f_ip)
+    output = xgb_model.predict(f_ip)[0]
+    return render_template("home.html", prediction_text="The predicted loan status is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
