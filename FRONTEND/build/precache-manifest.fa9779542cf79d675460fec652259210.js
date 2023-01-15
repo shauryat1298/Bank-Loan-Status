26a89c0e88242edbf726e0d517c6ea83ca2ceb09
sarrthak <sarthak.trip99@gmail.com>
2023-01-15 17:10:18 +0530

    Added Production build folder

---
+++ FRONTEND/build/precache-manifest.fa9779542cf79d675460fec652259210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cc7c2b7190af1782bf6",
+    "url": "/static/css/main.c63fa81e.chunk.css"
+  },
+  {
+    "revision": "2cc7c2b7190af1782bf6",
+    "url": "/static/js/main.2cc7c2b7.chunk.js"
+  },
+  {
+    "revision": "ff92814c08a423b03300",
+    "url": "/static/js/1.ff92814c.chunk.js"
+  },
+  {
+    "revision": "2c2e8c0be47c10136810",
+    "url": "/static/js/2.2c2e8c0b.chunk.js"
+  },
+  {
+    "revision": "ab4a39bbc6b5279c9a6a",
+    "url": "/static/js/runtime~main.ab4a39bb.js"
+  },
+  {
+    "revision": "fab42af757a0081695b32d73767948cc",
+    "url": "/index.html"
+  }
+];
